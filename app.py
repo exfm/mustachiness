678cd7bf95f01787ee4c350f7dabff039b0db779
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2011-11-05 15:33:14 -0400

    You can get a bunch of data and stuff now

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request, json
 from exfm import ExfmClient
 from werkzeug.routing import BaseConverter
 from werkzeug.urls import url_fix
+import urllib
+import urllib2
+from collections import OrderedDict
 
 app = Flask(__name__)
 
@@ -31,6 +34,38 @@ def api_search(q):
     return jsonify(results)
 
 
-@app.route('/api/data/<url:url>')
-def api_data(url):
-    return jsonify({'url': url})
+def search_en():
+    params = [
+        ('api_key', 'N6E4NIOVYMTHNDM8J'),
+        ('artist', request.values.get('artist')),
+        ('title', request.values.get('title')),
+        ('bucket', 'audio_summary'),
+        ('bucket', 'song_hotttnesss'),
+    ]
+    fp = urllib2.urlopen("http://developer.echonest.com/api/v4/song/search?%s" % urllib.urlencode(params))
+    data = fp.read()
+    fp.close()
+    return json.loads(data)
+
+
+def song_profile(*id):
+    params = [('api_key', 'N6E4NIOVYMTHNDM8J')]
+    [params.append(('id', _)) for _ in id]
+
+    fp = urllib2.urlopen("http://developer.echonest.com/api/v4/song/profile?%s" % urllib.urlencode(params))
+    data = fp.read()
+    fp.close()
+    return json.loads(data)
+
+
+@app.route('/api/data')
+def api_data():
+    song = search_en()['response']['songs'][0]
+    analysis_url = song['audio_summary']['analysis_url']
+    fp = urllib2.urlopen(analysis_url)
+    data = json.loads(fp.read())
+    fp.close()
+    song['loudness'] = OrderedDict([(item['start'], item['loudness_max'])
+         for item in data['segments']])
+
+    return jsonify({'song': song})
